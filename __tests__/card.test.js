2804985577e05ceebc0df2004e7a62b3dfcc8b3f
ashleyn <ashleynavo@googlemail.com>
2021-11-10 11:35:47 +0000

    added more methods and pseudo code

--- __tests__/card.test.js
+++ __tests__/card.test.js
@@ -1,6 +1,14 @@
 const Card = require("../src/card.js");
 
 describe("Card", () => {
+  let card;
+  let stat;
+
+  beforeEach(() => {
+    card = new Card("Charmander");
+    stat = 10;
+  });
+
   it("can be instantiated", () => {
     const card = new Card("Charmander");
 
--- src/card.js
+++ src/card.js
@@ -6,6 +6,8 @@ let pokemonId;
 let pokemonHealth;
 let pokemonType;
 
+// beed random to cycle through pokemons and randomnly choose 3 for player and opponent
+
 class Card {
   constructor() {
     // name for pokemon
@@ -14,9 +16,38 @@ class Card {
     this.pokemonId = pokemonId;
     this.pokemonHealth = pokemonHealth;
     this.pokemonType = pokemonType;
+    this.score = [];
   }
   // method to list stats
   listStats() {}
+
+  // method to draw card
+  drawCard() {}
+
+  // method to choose stat
+  chooseStat() {
+    prompt("Which stat do you want to play");
+  }
+
+  // method to compare stats
+  compareStat() {
+    const win = `You win with ${myStat}, your opponent's stat was ${oppStat}`;
+    const lose = `You lose! Your opponent's stat was ${oppStat}, your stat was ${myStat},`;
+    const draw = `It's a draw! Your stat: ${myStat}. Your opponent's stat ${oppStat}`;
+
+    if (myStat < oppStat) {
+      return win;
+    } else if (myStat > oppStat) {
+      return lose;
+    } else if (myStat == oppStat) {
+      return draw;
+    }
+  }
+
+  // method to store scores
+  storeScores(compareStat) {
+    this.score.push(compareStat);
+  }
 }
 
 module.exports = Card;
